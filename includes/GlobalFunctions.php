022b9c4370a0a29ab2302da7eba52ea3bcc9f6da
Vitaliy Filippov <vitalif@yourcmc.ru>
2015-10-09 20:19:46 +0300

    2.1.8 -- bug fixes and MediaWiki 1.25 support

--- includes/GlobalFunctions.php
+++ includes/GlobalFunctions.php
@@ -290,7 +290,7 @@ function haclfSetupExtension()
     //--- credits (see "Special:Version") ---
     $wgExtensionCredits['other'][] = array(
         'name'        => 'IntraACL',
-        'version'     => '2.1.7',
+        'version'     => '2.1.8',
         'author'      => "Vitaliy Filippov, Stas Fomin, Thomas Schweitzer",
         'url'         => 'http://wiki.4intra.net/IntraACL',
         'description' => 'The best MediaWiki rights extension, loosely based on HaloACL');
